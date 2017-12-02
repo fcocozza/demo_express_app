f7b053a844c87b286deca5d85e9c3950a1bdc5c6
Francisco Cocozza <fcocozza@gmail.com>
2017-12-02 13:21:56 -0600

    Fix to second line
    Fix to second line

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 var express = require('express');
 var app = express();
 app.get('/', function (req, res) {
-    res.send('Hello World! from demo_express_app by fcocozza (made with Node.js and Express) <br/> This is a new line');
+    res.send('Hello World! from demo_express_app by fcocozza (made with Node.js and Express) <br/>\
+     This is a new line in the response');
 });
 app.listen(3000, function () {
     console.log('Demonstration app listening on port 3000!');
