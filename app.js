dd70078ba9260341f682a28a86fdaebd18287caf
Francisco Cocozza <fcocozza@gmail.com>
2017-12-02 13:18:33 -0600

    Adding a new line to response
    Adding a new line to response

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 app.get('/', function (req, res) {
-    res.send('Hello World! from demo_app by fcocozza (made with Node.js and Express)');
+    res.send('Hello World! from demo_express_app by fcocozza (made with Node.js and Express) <br/> This is a new line');
 });
 app.listen(3000, function () {
     console.log('Demonstration app listening on port 3000!');
