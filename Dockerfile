bf09ae5dad50133871e88217b64904e1946afaf8
Francisco Cocozza <fcocozza@gmail.com>
2017-12-02 15:33:45 -0600

    Testing unique copy
    Without double copy commands in Dockerfile

--- Dockerfile
+++ Dockerfile
@@ -3,9 +3,10 @@ LABEL maintainer="fcocozza@gmail.com"
 RUN mkdir -p /demo_express_app
 WORKDIR /demo_express_app
 
-COPY package.json /demo_express_app
-RUN npm install express --save
+#COPY package.json /demo_express_app
 COPY . /demo_express_app
+RUN npm install express --save
+#COPY . /demo_express_app
 
 EXPOSE 3000
 
--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var app = express();
 app.get('/', function (req, res) {
     res.send('Hello World! from demo_express_app by fcocozza (made with Node.js and Express)<br/>\
      This is a new line in the response<br/>\
-     This is the third line!');
+     This is the third line! - without double copy');
 });
 app.listen(3000, function () {
     console.log('Demonstration app listening on port 3000!');
