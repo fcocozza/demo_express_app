3b319238d201cee6a419bca3f65602830277bada
Francisco Cocozza <fcocozza@gmail.com>
2017-12-02 16:58:23 -0600

    App.js -
    Adding style to HTML response

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 var express = require('express');
 var app = express();
 app.get('/', function (req, res) {
-    res.send('<h1>Hello World!<h1/>\
-    From demo_express_app by <b>fcocozza<b/> (made with <b>Node.js<b/> and <b>Express<b/>)');
+    res.send('<h1>Hello World!</h1>\
+    From demo_express_app by <b>fcocozza</b> (made with <b>Node.js</b> and <b>Express</b>)');
 });
 app.listen(3000, function () {
     console.log('Demonstration app listening on port 3000!');
