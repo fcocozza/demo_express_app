559d116967b4911cca86bf2378cfd4437072ae22
Francisco Cocozza <fcocozza@gmail.com>
2017-12-02 13:38:51 -0600

    Adding a third line
    Adding a third line

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 var express = require('express');
 var app = express();
 app.get('/', function (req, res) {
-    res.send('Hello World! from demo_express_app by fcocozza (made with Node.js and Express) <br/>\
-     This is a new line in the response');
+    res.send('Hello World! from demo_express_app by fcocozza (made with Node.js and Express)<br/>\
+     This is a new line in the response<br/>\
+     This is the third line!');
 });
 app.listen(3000, function () {
     console.log('Demonstration app listening on port 3000!');
