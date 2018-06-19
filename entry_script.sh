61a80b84059eae56250f5b9c43dc3170b52c2eb1
Francisco Cocozza <fcocozza@gmail.com>
2018-06-18 23:50:21 -0600

    Update entry_script.sh

--- entry_script.sh
+++ entry_script.sh
@@ -2,10 +2,10 @@
 echo $(date)
 echo "$CF_VOLUME_PATH"
 echo "$CF_REPO_NAME"
-ls $CF_VOLUME_PATH/$CF_REPO_NAME/
+#ls $CF_VOLUME_PATH/$CF_REPO_NAME/
 cp $CF_VOLUME_PATH/$CF_REPO_NAME/* /docker-entrypoint-initdb.d/
 echo 'ls /docker-entrypoint-initdb.d'
 ls /docker-entrypoint-initdb.d
-echo 'ls -alh /usr/local/bin/docker-entrypoint.sh'
-ls -alh /usr/local/bin/docker-entrypoint.sh 
+#echo 'ls -alh /usr/local/bin/docker-entrypoint.sh'
+#ls -alh /usr/local/bin/docker-entrypoint.sh 
 source /usr/local/bin/docker-entrypoint.sh
